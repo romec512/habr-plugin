586a11b951de032b7a771ba4f2cc17a90c383e95
Roman Asadov <roma-asadov@mail.ru>
2020-01-23 21:58:12 +0300

    Добавил получение содержимого статьи

--- content.js
+++ content.js
@@ -1,2 +1,6 @@
-console.log(document.getElementsByClassName("post__title-text")[0].innerHTML);
+let title = document.getElementsByClassName('post__title-text')[0].innerHTML;
+let articleBody = document.getElementByClassName('post__body post__body_full')[0].innerHTML;
+let authorName = document.getElementByClassName('user-info__fullname')[0].innerHTML;
+let authorNickname = document.getElementByClassName('user-info__nickname user-info__nickname_doggy')[0].innerHTML;
+
 
